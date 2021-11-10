1b2151effb4e6e50a318949ff91d61aeef5b8e6c
HARSHITKUCHHAL <harshitkuchhal1997@gmail.com>
2021-11-10 10:54:52 +0530

    COntrollers added Successfully

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
-const express=require('express')
-const router=express.Router()
-console.log('Router Loaded Successfully')
-module.exports=router
+const express = require("express");
+const router = express.Router();
+const homeController=require('../controllers/home_controller')
+
+
+console.log("Router Loaded Successfully");
+router.get('/',homeController.home)
+
+module.exports = router;
