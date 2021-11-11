37e66b3a96e1dac82d76cca8df28000d37a602c8
HARSHITKUCHHAL <harshitkuchhal1997@gmail.com>
2021-11-11 13:02:20 +0530

    Views are added successfully

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.use('/',require('./routes'))
+app.use("/", require("./routes"));
+app.set('view engine','ejs')
+app,use('views','./views')
 
 app.listen(port, function (err) {
   if (err) {
